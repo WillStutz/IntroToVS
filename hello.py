b1dfe3ce7aa217aaaad92701fdf0ba2d80f30bf7
Will <will_stutz1@baylor.edu>
2022-08-25 12:08:01 -0500

    intial commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+msg = "hello world"
+print(msg)
+
+list1 = [1,2,3,4,5,6]
+
+for i in list1:
+    print(i)
+    i += 1
---
+++ mygraph.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as pit
+import numpy as np
+
+x = np.linspace(0,20,100)
+pit.plot(x , np.sin(x))
+pit.show()
+
